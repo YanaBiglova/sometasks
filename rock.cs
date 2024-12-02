c92553fc8d7e2278d80e76a1f3d6bf1133a4c86b
YanaBiglova <yanabiglova9@gmail.com>
2024-12-02 18:39:53 +0500

    thats all

---
+++ rock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour {
+
+
+	private Transform _playerTransform;
+
+	void Start () {
+
+		_playerTransform = GameObject.FindWithTag("Player").transform;
+
+	}
+	
+
+	void Update () {
+
+
+		float distance = Vector3.Distance(transform.position, _playerTransform.position);
+
+		if (distance < 2.8f)
+        {
+			Destroy(gameObject);
+			FindObjectOfType<coinCounter>().Number += 1;
+        }
+	}
+}
+
